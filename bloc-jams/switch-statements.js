4e90664cf1b10fc6af87f1ff899b3b2c3e1b8645
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-10 04:52:33 -0500

    new pages to app

---
+++ bloc-jams/switch-statements.js
@@ -0,0 +1,35 @@
+theWeekend = function(week) {
+	switch (week) {
+		case Monday:
+		return "Nope";
+		break;
+
+		case Tuesday:
+		return "Nope again";
+		break;
+
+		case Wednesday:
+		return "Naw not yet";
+		break;
+
+		case Thursday:
+		return "One more day";
+		break;
+
+		case Friday:
+		return "Yeah buddy";
+		break;
+
+		case Saturday:
+		return "Its the WEEKEND!!!!";
+		break;
+
+		case Sunday:
+		return "Please dont end";
+		break;
+
+		default:
+		return "Not even close";
+		break;
+	}
+};
