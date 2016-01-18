091d6066a777194a0ce34232cb3eb4004618787e
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-17 22:09:40 -0500

    Assignment 23 completed

--- bloc-jams/scripts/landing.js
+++ bloc-jams/scripts/landing.js
@@ -1,26 +1,36 @@
 
-var revealPoint = function(points) {
-  var points = document.getElementsByClassName('point');
-  for (var i = 0; i < points.length; i++) {
-    points[i].style.opacity = 1;
-    points[i].style.transform = "scaleX(1) translateY(0)";
-    points[i].style.msTransform = "scaleX(1) translateY(0)";
-    points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
-  }
-};
-
-//The solution in the video is below. Mines seemed less complicated
-
-// var animatePoints = function() {
-// var revealPoint = function(index) {
-//   var points = document.getElementsByClassName('point');
-//   points[index].style.opacity = 1;
-//   points[index].style.transform = "scaleX(1) translateY(0)";
-//   points[index].style.msTransform = "scaleX(1) translateY(0)";
-//   points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-//
+// var revealPoint = function(points) {
 //   for (var i = 0; i < points.length; i++) {
-//     revealPoint();
+//     points[i].style.opacity = 1;
+//     points[i].style.transform = "scaleX(1) translateY(0)";
+//     points[i].style.msTransform = "scaleX(1) translateY(0)";
+//     points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
 //   }
 // };
-// };
+//
+
+
+var pointsArray = document.getElementsByClassName('point');
+
+var revealPoint = function(point) {
+    point.style.opacity = 1;
+    point.style.transform = "scaleX(1) translateY(0)";
+    point.style.msTransform = "scaleX(1) translateY(0)";
+    point.style.WebkitTransform = "scaleX(1) translateY(0)";
+};
+
+var animatePoints = function(points) {
+forEach(points, revealPoint);
+};
+
+window.onload = function() {
+  if (window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+
+  window.addEventListener('scroll', function(event) {
+    if (pointsArray[0].getBoundingClientRect().top <= 500) {
+        animatePoints(pointsArray);
+    }
+  });
+}
