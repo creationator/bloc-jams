dd132be540c96beb3de3f05524ba7ee63361d0dc
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-21 02:18:22 -0500

    Added play buttons to document

---
+++ egghead.js
@@ -0,0 +1,8 @@
+
+
+// JavaScript Array methods in depth - concat
+
+var items = [1, 2];
+var newItems = items.concat([3, 4], 5, 'string', undefined, ['string2', 'string3', 6, 7]);
+
+console.log(newItems);
