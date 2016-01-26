57d8f3e0ee40055d767de621b34344dba0c539d2
Derrick Morton <morton.derrick@gmail.com>
2016-01-25 23:56:32 -0500

    Delete egghead.js

--- egghead.js
+++
@@ -1,8 +0,0 @@
-
-
-// JavaScript Array methods in depth - concat
-
-var items = [1, 2];
-var newItems = items.concat([3, 4], 5, 'string', undefined, ['string2', 'string3', 6, 7]);
-
-console.log(newItems);
