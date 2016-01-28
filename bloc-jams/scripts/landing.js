51e9b688625ea6e73c6debd06f33a0b595977047
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-27 23:22:53 -0500

    cleaning up

--- bloc-jams/scripts/landing.js
+++ bloc-jams/scripts/landing.js
@@ -11,11 +11,11 @@
 
 var animatePoints = function() {
   var revealPoint = function() {
-           // #7
-           $(this).css({
-               opacity: 1,
-               transform: 'scaleX(1) translateY(0)'
-           });
+         // #7
+         $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
        };
        $.each($('.point'), revealPoint);
    };
