3f5fa95be8868461f3518cf53e080f455af0c156
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-27 23:45:24 -0500

    resolved merging issues by reverting back to previous checkpoint

--- bloc-jams/scripts/album.js
+++ bloc-jams/scripts/album.js
@@ -29,19 +29,6 @@ var albumPicasso = {
      ]
  };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-var createSongRow = function(songNumber, songName, songLength) {
-  var template =
-    '<tr class=album-view-song-item>'
-    + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-    + ' <td class="song-item-title">' + songName + '</td>'
-    + ' <td class="song-item-duration">' + songLength + '</td>'
-    + '</tr>'
-    ;
-=======
-=======
->>>>>>> checkpoint-31-playpause
  var createSongRow = function(songNumber, songName, songLength) {
    var template =
      '<tr class=album-view-song-item>'
@@ -50,14 +37,6 @@ var createSongRow = function(songNumber, songName, songLength) {
      + ' <td class="song-item-duration">' + songLength + '</td>'
      + '</tr>'
      ;
-<<<<<<< HEAD
-
-     return template;
-   };
->>>>>>> checkpoint-30-collection
-
-=======
->>>>>>> checkpoint-31-playpause
 
     var $row = $(template);
 
@@ -105,11 +84,8 @@ var createSongRow = function(songNumber, songName, songLength) {
 
    };
 
-<<<<<<< HEAD
-=======
 
 
->>>>>>> checkpoint-31-playpause
    var setCurrentAlbum = function(album) {
         // #1
      var $albumTitle = $('.album-view-title');
@@ -132,75 +108,6 @@ var createSongRow = function(songNumber, songName, songLength) {
           var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
           $albumSongList.append($newRow);
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-   };
-
-   // Album button templates
-   var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-   var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-=======
-    };
-
-    var findParentByClassName = function(element, targetClass) {
-      var currentParent = element.parentElement;
-      while (currentParent.className != targetClass) {
-        currentParent = currentParent.parentElement;
-      }
-      return currentParent;
-    };
-
-    var getSongItem = function(element) {
-      switch (element.className) {
-        case 'album-song-button':
-        case 'ion-play':
-        case 'ion-pause':
-         return findParentByClassName(element, 'song-item-number');
-       case 'album-view-song-item':
-         return element.querySelector('.song-item-number');
-       case 'song-item-title':
-       case 'song-item-duration':
-         return findParentByClassName(element, 'album-view-song-item').querySelector('.song-item-number');
-       case 'song-item-number':
-         return element;
-         default:
-           return;
-      }
-    };
-
-    var clickHandler = function(targetElement) {
-      var songItem = getSongItem(targetElement);
-
-      if (currentlyPlayingSong === null) {
-        songItem.innerHTML = pauseButtonTemplate;
-        currentlyPlayingSong = songItem.getAttribute('data-song-number');
-
-      } else if (currentlyPlayingSong === songItem.getAttribute('data-song-number')) {
-        songItem.innerHTML = playButtonTemplate;
-        currentlyPlayingSong = null;
-      } else if (currentlyPlayingSong !== songItem.getAttribute('data-song-number')) {
-          var currentlyPlayingSongElement = document.querySelector('[data-song-number="' + currentlyPlayingSong + '"]');
-          currentlyPlayingSongElement.innerHTML = currentlyPlayingSongElement.getAttribute('data-song-number');
-          songItem.innerHTML = pauseButtonTemplate;
-          currentlyPlayingSong = songItem.getAttribute('data-song-number');
-      }
-    };
-
-    var songListContainer = document.getElementsByClassName('album-view-song-list')[0];
-    // Elements we'll be adding listeners to
-    var songRows = document.getElementsByClassName('album-view-song-item');
-
-    // Album button templates
-    var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-    var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-
-    //Store state of playing songs
-    var currentlyPlayingSong = null;
-
-    window.onload = function() {
-        setCurrentAlbum(albumPicasso);
-        songListContainer.addEventListener('mouseover', function(event){
-=======
     };
 
 
@@ -210,49 +117,8 @@ var createSongRow = function(songNumber, songName, songLength) {
 
     //Store state of playing songs
     var currentlyPlayingSong = null;
->>>>>>> checkpoint-31-playpause
 
-         // Only target individual song rows during delegation
-         if (event.target.parentElement.className === 'album-view-song-item') {
-           // Change the content from the number to the play button's HTML
-           event.target.parentElement.querySelector('.song-item-number').innerHTML = playButtonTemplate;
-
-           var songItem = getSongItem(event.target);
->>>>>>> checkpoint-30-collection
-
-           if (songItem.getAttribute('data-song-number') !== currentlyPlayingSong) {
-             songItem.innerHTML = playButtonTemplate;
-           }
-         }
-        });
-
-<<<<<<< HEAD
     $(document).ready(function() {
-<<<<<<< HEAD
-       setCurrentAlbum(albumPicasso);
-   });
-=======
-        for (i = 0; i < songRows.length; i++) {
-          songRows[i].addEventListener('mouseleave', function(event) {
-            // Revert the content back to the number
-
-            var songItem = getSongItem(event.target);
-            var songItemNumber = songItem.getAttribute('data-song-number');
-
-            if (songItemNumber !== currentlyPlayingSong) {
-              songItem.innerHTML = songItemNumber;
-            }
-          });
-
-          songRows[i].addEventListener('click', function(event) {
-             // Event handler call
-             clickHandler(event.target);
-          });
-        }
-    };
->>>>>>> checkpoint-30-collection
-=======
         setCurrentAlbum(albumPicasso);
 
     });
->>>>>>> checkpoint-31-playpause
