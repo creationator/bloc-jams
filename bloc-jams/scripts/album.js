3bb53d535e519a95388ed84f841cf82aa7b853f0
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-27 23:41:21 -0500

    added fixes to album rendering issues

--- bloc-jams/scripts/album.js
+++ bloc-jams/scripts/album.js
@@ -29,15 +29,14 @@ var albumPicasso = {
      ]
  };
 
-var createSongRow = function(songNumber, songName, songLength) {
-  var template =
-    '<tr class=album-view-song-item>'
-    + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-    + ' <td class="song-item-title">' + songName + '</td>'
-    + ' <td class="song-item-duration">' + songLength + '</td>'
-    + '</tr>'
-    ;
-
+ var createSongRow = function(songNumber, songName, songLength) {
+   var template =
+     '<tr class=album-view-song-item>'
+     + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
+     + ' <td class="song-item-title">' + songName + '</td>'
+     + ' <td class="song-item-duration">' + songLength + '</td>'
+     + '</tr>'
+     ;
 
     var $row = $(template);
 
@@ -61,12 +60,12 @@ var createSongRow = function(songNumber, songName, songLength) {
     };
 
     var onHover = function(event) {
-    var songNumberCell = $(this).find('.song-item-number');
-    var songNumber = songNumberCell.attr('data-song-number');
+        var songNumberCell = $(this).find('.song-item-number');
+        var songNumber = songNumberCell.attr('data-song-number');
 
-    if (songNumber !== currentlyPlayingSong) {
-        songNumberCell.html(playButtonTemplate);
-      }
+        if (songNumber !== currentlyPlayingSong) {
+            songNumberCell.html(playButtonTemplate);
+        }
     };
 
     var offHover = function(event) {
@@ -78,42 +77,48 @@ var createSongRow = function(songNumber, songName, songLength) {
         }
     };
 
+
     $row.find('.song-item-number').click(clickHandler);
     $row.hover(onHover, offHover);
     return $row;
 
-  };
+   };
 
-  var setCurrentAlbum = function(album) {
-       // #1
-       var $albumTitle = $('.album-view-title');
-       var $albumArtist = $('.album-view-artist');
-       var $albumReleaseInfo = $('.album-view-release-info');
-       var $albumImage = $('.album-cover-art');
-       var $albumSongList = $('.album-view-cong-list');
 
-       $albumTitle.text(album.name);
-       $albumArtist.text(album.artist);
-       $albumReleaseInfo.text(album.year + ' ' + album.label);
-       $albumImage.attr('src', album.albumArtUrl);
 
-       // #3
-       $albumSongList.empty();
+   var setCurrentAlbum = function(album) {
+        // #1
+     var $albumTitle = $('.album-view-title');
+     var $albumArtist = $('.album-view-artist');
+     var $albumReleaseInfo = $('.album-view-release-info');
+     var $albumImage = $('.album-cover-art');
+     var $albumSongList = $('.album-view-song-list');
 
-       // #4
-       for (i = 0; i < album.songs.length; i++) {
+        // #2
+        $albumTitle.text(album.name);
+        $albumArtist.text(album.artist);
+        $albumReleaseInfo.text(album.year + ' ' + album.label);
+        $albumImage.attr('src', album.albumArtUrl);
+
+        // #3
+        $albumSongList.empty();
+
+        // #4
+        for (i = 0; i < album.songs.length; i++) {
           var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
           $albumSongList.append($newRow);
         }
-   };
+    };
 
-   // Album button templates
-   var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-   var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
 
-   //Store state of playing songs
-   var currentlyPlayingSong = null;
+    // Album button templates
+    var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
+    var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
+
+    //Store state of playing songs
+    var currentlyPlayingSong = null;
 
     $(document).ready(function() {
-       setCurrentAlbum(albumPicasso);
-   });
+        setCurrentAlbum(albumPicasso);
+
+    });
