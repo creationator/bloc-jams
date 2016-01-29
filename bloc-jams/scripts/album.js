2b3c62ca40aecfe52793cec4af48a8d580df75b2
Derrick Morton <derrick.mortion@datascantech.com>
2016-01-29 01:29:01 -0500

    Synched master with checkpoint branch

--- bloc-jams/scripts/album.js
+++ bloc-jams/scripts/album.js
@@ -1,45 +1,5 @@
 
 
-<<<<<<< HEAD
- var createSongRow = function(songNumber, songName, songLength) {
-   var template =
-     '<tr class=album-view-song-item>'
-     + ' <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
-     + ' <td class="song-item-title">' + songName + '</td>'
-     + ' <td class="song-item-duration">' + songLength + '</td>'
-     + '</tr>'
-     ;
-
-    var $row = $(template);
-
-    var clickHandler = function() {
-    	var songNumber = $(this).attr('data-song-number');
-
-    	if (currentlyPlayingSong !== null) {
-    		// Revert to song number for currently playing song because user started playing new song.
-    		var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSong + '"]');
-    		currentlyPlayingCell.html(currentlyPlayingSong);
-    	}
-    	if (currentlyPlayingSong !== songNumber) {
-    		// Switch from Play -> Pause button to indicate new song is playing.
-    		$(this).html(pauseButtonTemplate);
-    		currentlyPlayingSong = songNumber;
-    	} else if (currentlyPlayingSong === songNumber) {
-    		// Switch from Pause -> Play button to pause currently playing song.
-    		$(this).html(playButtonTemplate);
-    		currentlyPlayingSong = null;
-    	}
-    };
-
-    var onHover = function(event) {
-        var songNumberCell = $(this).find('.song-item-number');
-        var songNumber = songNumberCell.attr('data-song-number');
-
-        if (songNumber !== currentlyPlayingSong) {
-            songNumberCell.html(playButtonTemplate);
-        }
-    };
-=======
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -60,7 +20,6 @@ var createSongRow = function(songNumber, songName, songLength) {
               var currentlyPlayingCell = $('.song-item-number[data-song-number="' + currentlyPlayingSongNumber + '"]');
               currentlyPlayingCell.html(currentlyPlayingSongNumber);
       }
->>>>>>> checkpoint-32-next-prev
 
      if (currentlyPlayingSongNumber !== songNumber) {
           // Switch from Play -> Pause button to indicate new song is playing.
@@ -87,53 +46,6 @@ var createSongRow = function(songNumber, songName, songLength) {
       }
   };
 
-<<<<<<< HEAD
-
-    $row.find('.song-item-number').click(clickHandler);
-    $row.hover(onHover, offHover);
-    return $row;
-
-   };
-
-
-
-   var setCurrentAlbum = function(album) {
-        // #1
-     var $albumTitle = $('.album-view-title');
-     var $albumArtist = $('.album-view-artist');
-     var $albumReleaseInfo = $('.album-view-release-info');
-     var $albumImage = $('.album-cover-art');
-     var $albumSongList = $('.album-view-song-list');
-
-        // #2
-        $albumTitle.text(album.name);
-        $albumArtist.text(album.artist);
-        $albumReleaseInfo.text(album.year + ' ' + album.label);
-        $albumImage.attr('src', album.albumArtUrl);
-
-        // #3
-        $albumSongList.empty();
-
-        // #4
-        for (i = 0; i < album.songs.length; i++) {
-          var $newRow = createSongRow(i + 1, album.songs[i].name, album.songs[i].length);
-          $albumSongList.append($newRow);
-        }
-    };
-
-
-    // Album button templates
-    var playButtonTemplate = '<a class="album-song-button"><span class="ion-play"></span></a>';
-    var pauseButtonTemplate = '<a class="album-song-button"><span class="ion-pause"></span></a>';
-
-    //Store state of playing songs
-    var currentlyPlayingSong = null;
-
-    $(document).ready(function() {
-        setCurrentAlbum(albumPicasso);
-
-    });
-=======
   var offHover = function(event) {
       var songNumberCell = $(this).find('.song-item-number');
       var songNumber = parseInt(songNumberCell.attr('data-song-number'));
@@ -279,4 +191,3 @@ $(document).ready(function() {
      $nextButton.click(nextSong);
 
  });
->>>>>>> checkpoint-32-next-prev
